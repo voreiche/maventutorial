56d37f6c7d4aba0a8192e3cd0f628eab948e39db
Volker Reichel <volker.reichel@freenet.de>
2016-03-20 16:14:49 +0100

    Step2: Class with dependent library (log4j)

--- s1/src/main/java/de/vrsoft/maven/tutorial/App.java
+++ s1/src/main/java/de/vrsoft/maven/tutorial/App.java
@@ -1,13 +1,18 @@
 package de.vrsoft.maven.tutorial;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	private static final Logger LOGGER = Logger.getRootLogger();
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LOGGER.info("Hello World!" );
+        
     }
 }
